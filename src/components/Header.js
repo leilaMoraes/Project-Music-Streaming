db2bcd49deb7459b0f9d25231e8e3cc2d6d5781d
Leila Moraes <leila.violino@gmail.com>
2022-10-03 21:08:52 -0300

    conclusão do requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from '../pages/Loading';
 
@@ -25,6 +26,11 @@ class Header extends Component {
       <header
         data-testid="header-component"
       >
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </nav>
         <p
           data-testid="header-user-name"
         >
